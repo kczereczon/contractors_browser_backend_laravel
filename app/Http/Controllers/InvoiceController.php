184f2d20784a03a52093abdb17f68df728edbd76
Krzysztof Czereczon <kczereczon@gmail.com>
2021-01-16 03:07:26 +0100

    Fixed external api fetcher

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -13,17 +13,19 @@ class InvoiceController extends Controller
     {
         $apiBase = env("INVOICES_API_BASE", null);
 
-        if($apiBase) {
-            return Http::get(
-                $apiBase."/".$contractor['nip'],
+        if (!empty($apiBase)) {
+            $response = Http::get(
+                $apiBase . "/" . $contractor['nip'],
                 [
                     "date_from" => $request->date_from,
                     "date_to" => $request->date_from,
                 ]
-        );
+
+            );
+            return $response->json();
         } else {
             $requestCall = Request::create(
-                "/api/faked/invoices/".$contractor['nip'],
+                "/api/faked/invoices/" . $contractor['nip'],
                 'GET',
                 [
                     "date_from" => $request->date_from,
